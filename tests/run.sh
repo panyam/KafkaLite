e8723468d35b155b63b909a5717760c616e4cb98
Sriram Panyam <sri.panyam@gmail.com>
2015-06-28 22:14:52 +0000

    main.cc to enable c++

--- tests/run.sh
+++ tests/run.sh
@@ -4,5 +4,5 @@ rm -rf ../bld ; mkdir -p ../bld ; cd ../bld
 gcc -Wall -g -c -std=gnu99 ../src/*.c
 ar -cvq libkafkalite.a *.o
 cd -
-g++ -g *.cc main.c -I ../src/ ../bld/*.o -o ../bld/kltests -lcppunit  -lpthread
+g++ -g *.cc main.cc -I ../src/ ../bld/*.o -o ../bld/kltests -lcppunit  -lpthread
 ../bld/kltests
